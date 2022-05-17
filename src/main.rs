45fc8a47c5da2fe2cf092df8101934703ea94411
Ziga Pregelj <pregelj.ziga@gmail.com>
2022-05-17 03:41:51 +0200

    Csv parse

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
+use serde::{Serialize, Deserialize};
+use std::error::Error;
+use std::io;
+use std::process;
+
 
 #[derive(Debug, Deserialize)]
 struct Transaction {
     action: String,
     client_id: u16,
     tx_id: u32,
-    amount: f32
+    amount: Option<f32>
 }
 
 
+
+fn parse_csv() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path("tests/test.csv")?;
+    for result in rdr.deserialize() {
+        let record: Transaction = result?;
+        let x = match record.client_id {
+            1 => println!("pog"),
+            _ => {}
+        };
+
+        // parse csv ->
+        // create clients with transactions ->
+        // pass it to calculator ->
+        // calculate for each client and write to csv
+
+        println!("{:?}", x);
+        //println!("{:?}", record.action);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = parse_csv() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
---
+++ tests/csv_parse.rs
@@ -0,0 +1,4 @@
+// parse csv ->
+// create clients with transactions ->
+// pass it to calculator ->
+// calculate for each client and write to csv
