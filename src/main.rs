6f0d24c180b12a973ca99d46376cf203296b44bf
Ziga Pregelj <pregelj.ziga@gmail.com>
2022-05-17 17:13:26 +0200

    vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 use std::error::Error;
 use std::io;
@@ -12,25 +13,31 @@ struct Transaction {
     amount: Option<f32>
 }
 
+enum Action {
+    Increment {amount: f32},
+    Decrement {amount: f32},
+    Dispute {amount: Option<f32>},
+    Resolve {amount: Option<f32>},
+    Chargeback {amount: Option<f32>}
+}
 
-
+// parse csv ->
+// create clients with transactions ->
+// pass it to calculator ->
+// calculate for each client and write to csv
 fn parse_csv() -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_path("tests/test.csv")?;
+    let mut groups: Vec<Transaction> = Vec::new();
     for result in rdr.deserialize() {
         let record: Transaction = result?;
-        let x = match record.client_id {
-            1 => println!("pog"),
-            _ => {}
-        };
-
-        // parse csv ->
-        // create clients with transactions ->
-        // pass it to calculator ->
-        // calculate for each client and write to csv
-
-        println!("{:?}", x);
+        groups.push(record);
+        //println!("{:?}", record);
         //println!("{:?}", record.action);
     }
+    let sorted = groups.into_iter().map(|x| x.client_id).collect();
+
+
+    println!("{:?}", sorted);
     Ok(())
 }
 
